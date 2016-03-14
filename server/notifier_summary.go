21894e255018f291528d3c8ab1fcf8896c5edd41
Thorsten Ball <mrnugget@gmail.com>
2016-03-14 10:32:01 +0100

    Add DeploymentURL() method to DeploymentEvent

--- server/notifier_summary.go
+++ server/notifier_summary.go
@@ -10,22 +10,12 @@ import (
 )
 
 func generateSummary(t *template.Template, ev *DeploymentEvent) (string, error) {
-	var scheme string
-	if config.SSLEnabled {
-		scheme = "https"
-	} else {
-		scheme = "http"
-	}
-
 	var success bool
 	if ev.State == models.DEPLOYMENT_SUCCESSFUL {
 		success = true
 	} else {
 		success = false
 	}
-
-	deploymentUrl := fmt.Sprintf("%s://%s/%v/deployments/%v", scheme, config.Host,
-		ev.Application.GitHubRepo, ev.Deployment.Id)
 
 	gitHubUrl := fmt.Sprintf("https://github.com/%v/%v/commit/%v",
 		ev.Application.GitHubOwner, ev.Application.GitHubRepo,
@@ -41,7 +31,7 @@ func generateSummary(t *template.Template, ev *DeploymentEvent) (string, error) 
 		"Comment":       ev.Deployment.Comment,
 		"CommentLines":  strings.Split(ev.Deployment.Comment, "\n"),
 		"GitHubUrl":     gitHubUrl,
-		"DeploymentURL": deploymentUrl,
+		"DeploymentURL": ev.DeploymentURL(),
 	})
 
 	return summary.String(), err
